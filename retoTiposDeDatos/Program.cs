eeb700de42cc5544997059403870516192882f98
LPAMENDEZ <panessoliliana785@gmail.com>
2026-02-14 12:18:17 -0500

    Subiendo proyecto

---
+++ retoTiposDeDatos/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Timers;
+
+namespace retoTiposDeDatos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // variable entera 
+
+            int age = 25;
+
+            // variable decimal
+
+            double height = 7.75;
+
+            // variable boleana
+
+            bool is_student = true;
+
+            //variable de texto
+
+            string name = "Ana";
+
+            // variable de tipo caracter 
+
+            char grade = 'A';
+
+
+            // imprimir 
+
+            Console.WriteLine("la edad es: " + age);
+            Console.WriteLine("el tamaño es: " + height);
+            Console.WriteLine("ella es estudiante: " + is_student);
+            Console.WriteLine("su nombre es: " + name);
+            Console.WriteLine("el caracter es: " + grade);
+
+
+
+
+        }
+    }
+}
